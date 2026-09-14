e8afd1f9982dde90203f4a49cd67ffa951366a92
Gavriel <253627515+GavrielKnight@users.noreply.github.com>
2026-09-13 23:05:55 -0600

    Incluye elementos
    Logo de la app, barra de busqueda y mas videos

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import { View, Text, Button, Pressable } from "react-native";
+import { View, Text, Button, Pressable, TextInput } from "react-native";
+import { Image } from "expo-image";
 import { styles } from "./styles.js"
 
 export default function Layout({ children }) {
@@ -6,14 +7,13 @@ export default function Layout({ children }) {
         <View style={{ flex: 1 }}>
 
             {/* Barra superior */}
-            <View style={styles.bars}>
-                <Text style={{
-                    color: "white",
-                    fontSize: 22,
-                    fontWeight: "bold"
-                }}>
-                    Video App
-                </Text>
+            <View style={styles.upperBar}>
+                <Image source={require("../../assets/images/expo-logo.png")}
+                style={styles.logo}/>
+                <Text style={styles.appTitle}>Video App</Text>
+                <TextInput style={styles.searchBox} placeholder="Search video..."/>
+                <Image source={require("../../assets/images/search-icon.svg")}
+                style={styles.icon}/>
             </View>
 
             {/* Contenido */}
@@ -26,15 +26,15 @@ export default function Layout({ children }) {
             </View>
 
             {/* Barra inferior */}
-            <View style={styles.bars}>
+            <View style={styles.bottomBar}>
                 <Pressable style={styles.menuButton}>
                     <Text style={styles.menuText}>Inicio</Text>
                 </Pressable>
                 <Pressable style={styles.menuButton}>
-                    <Text style={styles.menuText}>Alumnos</Text>
+                    <Text style={styles.menuText}>Listas de Reproducción</Text>
                 </Pressable>
                 <Pressable style={styles.menuButton}>
-                    <Text style={styles.menuText}>Materias</Text>
+                    <Text style={styles.menuText}>Perfil</Text>
                 </Pressable>
             </View>
         </View>
--- src/app/styles.js
+++ src/app/styles.js
@@ -25,19 +25,43 @@ export const styles = StyleSheet.create({
         fontSize: 20,
     },
 
-    bars: {
+    upperBar: {
+        height: 80,
+        backgroundColor: "#e12222",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "row",
+        gap: 20,
+    },
+    bottomBar: {
         height: 80,
         backgroundColor: "#e12222",
         justifyContent: "space-evenly",
         alignItems: "center",
         flexDirection: "row",
     },
+    logo: {
+        height: 30,
+        width: 30,
+    },
+    icon: {
+        height: 30,
+        width: 30,
+    },
+    appTitle: {
+        fontSize: 20,
+        color: "white",
+        fontWeight: "bold",
+    },
+    searchBox: {
+        backgroundColor: "white"
+    },
     menuButton: {
         backgroundColor: "transparent",
         padding: 10,
     },
     menuText: {
-        fontSize: 20,
+        fontSize: 16,
         color: "white",
         fontWeight: "bold",
     }
